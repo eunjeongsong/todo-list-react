2a4bdb3c9c5b301ebf6c8f0eeb7e145596da68f4
eunjeongsong <hellogine12@gmail.com>
2018-08-22 02:17:24 +0900

    creat todolist react app

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+    render() {
+        const { value, onChange, onCreate, onKeyPress } = this.props;
+        
+        return (
+            <div className = "form">
+                <input value={value} onChange={onChange} onKeyPress={onKeyPress} />
+                <div className="create-button" onClick={onCreate}>
+                    추가
+                </div>
+            </div>
+        );
+    };
+}
+
+export default Form;
